98cf974e875b5b18deffb180fc2b2e4bfceea05c
Natasha Farias <nataschafarias@gmail.com>
2025-12-01 03:17:20 +0000

    Add validate_date.py script

---
+++  validate_date.py
@@ -0,0 +1,14 @@
+import re
+
+def validate_date(date):
+    pattern = r"\b(0?[1-9]|1[012])[-/( ](0?[1-9]|[12][0-9]|3[01])[/- ](\d{4})\b"
+    if re.match(pattern, date):
+        return True
+    else:
+        return False
+
+date = input("Enter a date (DD/MM/YYYY): ")
+if validate_date(date):
+    print(f"{date} is a valid date!")
+else:
+    print(f"{date} is not a valid date.")
